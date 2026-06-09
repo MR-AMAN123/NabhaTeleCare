9c8dc21c16826b9ee70d79afa9f662e523a2ac06
Mohd_Aman <mohd2310021@akgec.ac.in>
2026-06-09 18:24:37 +0530

    Prepare backend for Vercel

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error.message);
-    process.exit(1);
+   throw error;
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -72,8 +72,4 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
