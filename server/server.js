549766249fb61394dc8597aea7ee2addf02e38a3
Mohd_Aman <mohd2310021@akgec.ac.in>
2026-06-11 11:34:27 +0530

    fix render deploy

--- server/server.js
+++ server/server.js
@@ -35,7 +35,15 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://nabha-tele-care-obei.vercel.app"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -72,4 +80,8 @@ app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
